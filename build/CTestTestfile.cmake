1d272a56d01bd0b2e149ae72224b74c1bb80eced
jesseosu <jesse.osuna96@gmail.com>
2026-02-04 14:52:30 +1100

    chore: ignore build artifacts and generated files

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/roast/Documents/GitHub/celeritas
-# Build directory: C:/Users/roast/Documents/GitHub/celeritas/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
